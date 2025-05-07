d58e37d8d84067d1ab94e0face1dd959f58f611c
Anna Tiberg <anastasia-elisabeth.tiberg@elev.ga.ntig.se>
2025-05-07 11:02:13 +0200

    server klar

--- src/MulticastChatServer.java
+++ src/MulticastChatServer.java
@@ -1,3 +1,7 @@
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
 public class MulticastChatServer {
 
     public static void main(String args[])
@@ -8,5 +12,27 @@ public class MulticastChatServer {
     if (args.length >= 1){
         portnumber = Integer.parseInt(args[0]);
     }
+
+    //create multicastSocket
+    MulticastSocket serverMulticastSocket = new MulticastSocket(portnumber);
+        System.out.println("MulticastSocket is created at port " + portnumber);
+
+    //Determine the IP address of a host, given the host name
+        InetAddress group = InetAddress.getByName("225.4.5.6")  ;
+
+    //GetByName- returns IP address of given host
+    serverMulticastSocket.joinGroup(group);
+        System.out.println("joinGroup method is called...");
+        boolean infinite = true;
+
+    //Continually recieves data and prints them
+    while (infinite){
+        byte buf[] = new byte[1024];
+        DatagramPacket data = new DatagramPacket(buf, buf.length);
+        serverMulticastSocket.receive(data);
+        String msg = new String(data.getData()).trim();
+        System.out.println("Message received from client = "+ msg);
+    }
+    serverMulticastSocket.close();
     }
 }
